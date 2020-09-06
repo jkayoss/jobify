fa512298d021f2258528cbda6449b890815b75a9
Jhonatan Kayo <kayojhonatan@gmail.com>
2020-09-06 14:25:10 -0300

    atualizando a segurança para acesso ao admin

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.get('/admin', (req, res, next) => {
+    if(req.hostname === 'localhost') {
+        next()
+    } else {
+        res.render('Você não tem permissão para fazer isso :)')
+    }
+})
+
 app.get('/', async(request, response) => {
     const db = await dbConnection
     const categoriasdb = await db.all('select * from categorias')
