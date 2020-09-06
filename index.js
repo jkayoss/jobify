c862ab7bea7a8c5e16b3606489553ec1f0103888
Jhonatan Kayo <kayojhonatan@gmail.com>
2020-09-06 12:49:33 -0300

    Adicionada nova configuração de porta

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const sqlite = require('sqlite')
 const dbConnection = sqlite.open('banco.sqlite', { Promise })
 
+const port = process.env.PORT || 3000
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -89,7 +91,7 @@ const init = async() => {
 }
 init()
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
     if(err) {
         console.log('erro na inicialização')
     } else {
